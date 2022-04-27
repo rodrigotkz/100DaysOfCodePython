1f499f1e1f224b33088a8001d5312207a4351311
RodrigoTakazono <digotakazono@hotmail.com>
2022-04-27 17:36:20 -0300

    BandNameGenerator

---
+++ day1/main.py
@@ -0,0 +1,7 @@
+# Trabalhando com entrada Input, e Função Len, para contagem de caracteres.
+print(len(input("What is your name? ")))
+
+#Criando uma variavel
+name = input("What is your name?") 
+length = len(name)
+print(length)
---
+++ day1/project.py
@@ -0,0 +1,7 @@
+#Projeto day1 
+print("Bem-Vindos ao gerador de nome de Bandas!!")
+
+cidade = input("Qual cidade você nasceu? \n")
+nome_pet = input("Qual o nome de seu pet? \n")
+
+print("O nome da sua banda é: " + cidade + " " + nome_pet )
